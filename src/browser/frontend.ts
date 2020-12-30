6fb2628982f9f398e71c78ccabd9fa174893d6b5
yeweiasia <yeweiasia@gmail.com>
2020-12-30 20:10:03 +0800

    add example of cloudide event subscription

---
+++ src/browser/frontend.ts
@@ -0,0 +1,62 @@
+/********************************************************************************
+ * Copyright (C) 2020 yewei All rights reserved.
+ * SPDX-License-Identifier: MIT
+ ********************************************************************************/
+
+import { LogLevel, EventType } from '@cloudide/core/lib/common/plugin-common';
+import { PluginPage, AbstractFrontend } from '@cloudide/core/lib/browser/plugin-api';
+import { exposable, expose } from '@cloudide/messaging';
+
+/**
+ * Adding your fronted api in this class
+ * Using '@expose' to expose your function to backend
+ */
+@exposable
+class Frontend extends AbstractFrontend {
+
+    /**
+     * function call to the frontend will wait until init() to be resolved
+     */
+    async init(): Promise<void> {
+
+    }
+
+    /**
+     * Entry of your plugin frontend
+     * In this function your can call function exposed by backend
+     */
+    run(): void {
+        const receivedEventsDom = document.getElementById('received-event');
+
+        this.plugin.subscribeEvent(EventType.CLOUDIDE_WORKSPACE_ONDIDCREATEFILES, (eventType, evt) => {
+            const receivedEvent = document.createElement('pre');
+            receivedEvent.append(document.createTextNode(`${JSON.stringify(evt)}`));
+            receivedEventsDom?.appendChild(receivedEvent);
+        });
+    }
+
+    stop(): void {
+
+    }
+
+    /**
+     * this function can be called from plugin backend as below:
+     * @example
+     * ```
+     * plugin.call('myplugin.page.myApi', 'this is a function call from backend').then(ret => {
+     *     console.log(ret);
+     * });
+     * 
+     * ```
+     */
+    @expose('myplugin.page.myApi')
+    public myApi(message: string): string {
+        console.log(message);
+        return 'this is a return value from frontend function';
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    PluginPage.create([Frontend]);
+});
---
+++ src/node/backend.ts
@@ -0,0 +1,37 @@
+/********************************************************************************
+ * Copyright (C) 2020 yewei All rights reserved.
+ * SPDX-License-Identifier: MIT
+ ********************************************************************************/
+
+import * as cloudide from '@cloudide/plugin';
+import { exposable, expose } from '@cloudide/messaging';
+import { LogLevel } from '@cloudide/core/lib/common/plugin-common';
+import { AbstractBackend } from '@cloudide/core/lib/node/plugin-api';
+
+/**
+ * Add your backend api in this class
+ * Using '@expose' to expose your function to frontend
+ */
+@exposable
+export class Backend extends AbstractBackend {
+
+    /**
+     * function call to the backend will wait until init() to be resolved
+     */
+    async init(): Promise<void> {
+
+    }
+
+    /**
+     * Entry of your plugin backend
+     * In this function you can call function exposed by frontend 
+     */
+    public async run(): Promise<void> {
+
+    }
+
+    public stop(): void {
+
+    }
+
+}
