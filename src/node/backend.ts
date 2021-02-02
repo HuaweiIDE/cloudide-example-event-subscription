6e6eeca1623feff609d1b0e37825ddf771dfc4ce
admaxone <admaxone@example.com>
2021-02-02 14:56:20 +0800

    add deleteFiles Event listerner on backend

--- src/node/backend.ts
+++ src/node/backend.ts
@@ -27,7 +27,9 @@ export class Backend extends AbstractBackend {
      * In this function you can call function exposed by frontend 
      */
     public async run(): Promise<void> {
-
+        this.plugin.container.context.subscriptions.push(cloudide.workspace.onDidDeleteFiles((event) => {
+            cloudide.window.showInformationMessage(`${event.files.join(',')} deleted.`);
+        }));
     }
 
     public stop(): void {
